326299bc8f2f6b08cdf11936b81807336730aab3
Trever Fischer <tdfischer@fedoraproject.org>
2012-08-02 14:12:42 -0400

    Add the publish script we use on synhak.org

---
+++ tools/publish.sh
@@ -0,0 +1,16 @@
+# Used on synhak.org to publish files to http://synhak.org/~secretary/
+
+cd /home/secretary/synhak-documents/latex
+git pull
+git checkout -f master
+make
+rsync -avz *.pdf --delete /home/secretary/public_html/docs/
+
+cd /home/secretary/synhak-documents/svg
+rsync -avz *.pdf *.png *.svg --delete /home/secretary/public_html/docs/images/
+
+cd /home/secretary/synhak-bylaws/
+git pull
+git checkout -f master
+make
+cp *.pdf /home/secretary/public_html/docs/

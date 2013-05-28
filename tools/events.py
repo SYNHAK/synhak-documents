678ed14446d71fd0373a27f8140118ee7a8217b6
Torrie Fischer <wm161@wm161.net>
2013-05-28 17:57:58 -0400

    Also update the wiki

--- tools/events.py
+++ tools/events.py
@@ -14,6 +14,7 @@ parser = optparse.OptionParser()
 parser.add_option("-s", "--state-dir", help="Path to the directory where state is stored between runs", default="~/.local/share/Phong/", metavar="PATH")
 parser.add_option("-d", "--dry-run", help="Don't actually send mails or edit pages. Just say what would happen.", default=False, action="store_true")
 parser.add_option("-p", "--period", help="Days to look ahead (0 for today)", default=7)
+parser.add_option("-w", "--wiki", help="Update the wiki", default=False, action="store_true")
 parser.add_option("-e", "--mail", help="Address to send mail to. May be specified multiple times.", default=[], action="append", type="str", dest="mail_addresses")
 
 (options, args) = parser.parse_args()
@@ -33,7 +34,7 @@ for e in spiffAPI.events():
   if now < end and now+datetime.timedelta(days=period) > start:
     upcoming.append(e)
 
-if len(upcoming) > 0 and len(options.mail_addresses):
+if len(upcoming) > 0:
   params = {}
   params['greeting'] = api.randomGreeting()
   params['period'] = period
@@ -43,15 +44,26 @@ if len(upcoming) > 0 and len(options.mail_addresses):
       organizers.append("%s %s"%(u['user']['first_name'], u['user']['last_name']))
     evt['organizers'] = ', '.join(organizers)
   params['upcoming'] = upcoming
-  template = synhak.TemplatePage(api, "PhongTemplates/EventMail", params)
-  msg = MIMEText(unicode(template))
-  msg['Subject'] = "Upcoming Events"
-  msg['From'] = 'phong@synhak.org <Phong>'
-  msg['To'] = ','.join(options.mail_addresses)
-  print "Sending mail to", msg['To']
-  if not options.dry_run:
-    s = smtplib.SMTP('localhost')
-    s.sendmail(msg['From'], options.mail_addresses, msg.as_string())
-    s.quit()
-  else:
-    print msg.as_string()
+
+  if options.wiki:
+    reportTemplate = synhak.TemplatePage(api, "PhongTemplates/EventList", params)
+    reportPage = api.getPage("PhongPages/EventList")
+    print "Updating [[PhongPages/EventList]]"
+    if not options.dry_run:
+      reportPage.edit(summary="Event Update", bot=True, text=unicode(reportTemplate))
+    else:
+      print reportTemplate
+
+  if len(options.mail_addresses) > 0:
+    mailTemplate = synhak.TemplatePage(api, "PhongTemplates/EventMail", params)
+    msg = MIMEText(unicode(mailTemplate))
+    msg['Subject'] = "Upcoming Events"
+    msg['From'] = 'phong@synhak.org <Phong>'
+    msg['To'] = ','.join(options.mail_addresses)
+    print "Sending mail to", msg['To']
+    if not options.dry_run:
+      s = smtplib.SMTP('localhost')
+      s.sendmail(msg['From'], options.mail_addresses, msg.as_string())
+      s.quit()
+    else:
+      print msg.as_string()
